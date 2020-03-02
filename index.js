0040f0092113f64e385ba0ed8cf2a172cbafe1d2
andrew15722 <andrew15722@yahoo.com>
2020-03-02 00:59:39 -0700

    moved sqlite3 from devdependencies back to dependencies

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ require('dotenv').config();
 
 const server = require('./server.js');
 
-// todo add env tool for heroku
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
 	console.log(`server running on http://localhost:${port}`);
