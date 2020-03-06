abf4f35661f260af6be9d182eea8153cbf191820
andrew15722 <andrew15722@yahoo.com>
2020-03-05 23:36:32 -0700

    made the deployment run off of the production env

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,5 @@
 const knex = require('knex');
-const configOptions = require('../knexfile.js');
+const config = require('../knexfile.js');
 
 const dbEnv = process.env.DB_ENV || 'development';
-module.exports = knex(configOptions);
+module.exports = knex(config[dbEnv]);
